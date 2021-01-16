5c8cdc799b358dbd733cf46d36c4c5bc0c6e94f8
jolmstead <jl260629@dal.ca>
2021-01-16 13:33:05 -0400

    Add square functionality, add layout changes, change button design

--- app/src/main/java/ca/dal/cs/csci3130/a1/MainActivity.java
+++ app/src/main/java/ca/dal/cs/csci3130/a1/MainActivity.java
@@ -3,6 +3,10 @@ package ca.dal.cs.csci3130.a1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button squareButton = (Button)findViewById(R.id.squareButton);
+
+        squareButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                EditText numberEditField = (EditText)findViewById(R.id.numberEditField);
+                TextView resultView = (TextView) findViewById((R.id.textViewResult));
+
+                int numberToBeSquared =  Integer.parseInt(numberEditField.getText().toString());
+                int squaredNumber = numberToBeSquared * numberToBeSquared;
+
+                resultView.setText(squaredNumber+"");
+
+            }
+        });
+
     }
 }
